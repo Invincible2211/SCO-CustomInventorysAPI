0208c39182cdfeb74d8b87d636f1e52a48d7f645
Freddyblitz <fynn.schur@web.de>
2022-01-16 21:14:56 +0100

    create package structure; add main class

---
+++ src/main/java/de/fynn/sco/custominventoryapi/CustomInventoryAPI.java
@@ -0,0 +1,17 @@
+package de.fynn.sco.custominventoryapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomInventoryAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
